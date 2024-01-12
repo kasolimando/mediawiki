280feaa7ec663be1078bb8ada090a5feb0252381
Katherin Rodriguez <kasolimando@gmail.com>
2024-01-12 19:27:09 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,31 +17,6 @@ if [ ! -f "/var/www/html/LocalSettings.php" ]; then
         --lang=es \
         "MediwikiEquipo1" \
         "Admin"
-
-    echo -e '\t$wgUploadPath = "$wgScriptPath/images";' >> /var/www/html/LocalSettings.php
-
-    echo -e '\t$wgUploadDirectory = "/var/www/html/images";' >> /var/www/html/LocalSettings.php
-
-    echo -e '\t$wgResourceBasePath = $wgScriptPath;' >> /var/www/html/LocalSettings.php
-
-    echo -e '\t$wgLogos = [
-                        '1x' => "$wgResourceBasePath/resources/assets/change-your-logo.svg",
-                        'icon' => "$wgResourceBasePath/resources/assets/change-your-logo-icon.svg",
-                        ];' >> /var/www/html/LocalSettings.php
-
-    echo -e '\t$wgGroupPermissions['user']['upload'] = true;
-             \t$wgGroupPermissions['uploadaccess']['upload'] = true;
-             \t$wgGroupPermissions['autoconfirmed']['upload'] = false;
-             \t$wgGroupPermissions['user']['reupload'] = true;
-             \t$wgGroupPermissions['autoconfirmed']['reupload'] = false;
-             \t$wgGroupPermissions['user']['reupload-shared'] = true;
-             \t$wgFileExtensions = [ 'png', 'gif', 'jpg', 'jpeg', 'doc',
-                        'xls', 'mpp', 'pdf', 'ppt', 'tiff', 'bmp', 'docx', 'xlsx',
-                        'pptx', 'ps', 'odt', 'ods', 'odp', 'odg'
-                ];' >> /var/www/html/LocalSettings.php
-
-    echo -e '\t$wgEnableUploads = true; 
-             \t$wgUseImageMagick = true;' >> /var/www/html/LocalSettings.php
-
-    echo -e '\t$wgShowExceptionDetails = true;' >> /var/www/html/LocalSettings.php
 fi
+
+exec "$@"
