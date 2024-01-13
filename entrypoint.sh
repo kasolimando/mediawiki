be3004b0b963f0b888218bf57c46804c1fbdf441
Katherin Rodriguez <kasolimando@gmail.com>
2024-01-12 21:32:52 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,53 @@
+#!/bin/bash
+
 set -e
 
 if [ ! -f "/var/www/html/LocalSettings.php" ]; then
   echo "LocalSettings.php not found. Running MediaWiki setup..."
   php maintenance/install.php \
-        --confpath=/var/www/html \
-        --dbserver=mediawiki-database-1 \
-        --installdbuser=wikimedia \
-        --installdbpass=wikimedia \
-        --dbtype=mysql \
-        --dbname=wiki_db \
-        --dbuser=wikimedia \
-        --dbpass=wikimedia \
-        --pass=123456789CKpocho \
-        --server=http://192.168.56.2:8080 \
+        --confpath=$CONF_PATH \
+        --dbserver=$DBSERVER \
+        --installdbuser=$INSTALL_USER \
+        --installdbpass=$INSTALL_PASS \
+        --dbtype=$DBTYPE \
+        --dbname=$DBNAME \
+        --dbuser=$DBUSER \
+        --dbpass=$DBPASS \
+        --pass=$PASS \
+        --server=$SERVER \
         --scriptpath="" \
         --lang=es \
-        "MediwikiEquipo1" \
-        "Admin"
+        $MEDIAWIKI_NAME \
+        $MEDIAWIKI_USER
+
+  echo -e '$wgUploadPath = "$wgScriptPath/images";' >> /var/www/html/LocalSettings.php
+
+  echo -e '$wgUploadDirectory = "/var/www/html/images";' >> /var/www/html/LocalSettings.php
+
+  echo -e '$wgLogos = ["1x" => "$wgResourceBasePath/resources/assets/change-your-logo.svg","icon" => "$wgResourceBasePath/resources/assets/change-your-logo-icon.svg",]' >> /var/www/html/LocalSettings.php
+  
+  echo -e '$wgGroupPermissions["user"]["upload"] = true;' >> /var/www/html/LocalSettings.php
+
+  echo -e '$wgGroupPermissions["uploadaccess"]["upload"] = true;' >> /var/www/html/LocalSettings.php
+
+  echo -e '$wgGroupPermissions["autoconfirmed"]["upload"] = false;' >> /var/www/html/LocalSettings.php
+
+  echo -e '$wgGroupPermissions["user"]["reupload"] = true;' >> /var/www/html/LocalSettings.php
+
+  echo -e '$wgGroupPermissions["autoconfirmed"]["reupload"] = false;' >> /var/www/html/LocalSettings.php
+
+  echo -e '$wgGroupPermissions["user"]["reupload-shared"] = true;' >> /var/www/html/LocalSettings.php
+  
+  echo -e ' $wgFileExtensions = [ "png", "gif", "jpg", "jpeg", "doc","xls", "mpp", "pdf","ppt", "tiff", "bmp", "docx", "xlsx","pptx", "ps", "odt", "ods", "odp","odg"];' >> /var/www/html/LocalSettings.php
+  
+  echo -e '$wgEnableUploads = true;' >> /var/www/html/LocalSettings.php
+
+  echo -e '$wgUseImageMagick = true;' >> /var/www/html/LocalSettings.php
+
+  echo -e '$wgImageMagickConvertCommand = "/usr/bin/convert";' >> /var/www/html/LocalSettings.php
+
+  echo -e '$wgShowExceptionDetails = true;'  >> /var/www/html/LocalSettings.php
+
 fi
 
 exec "$@"
